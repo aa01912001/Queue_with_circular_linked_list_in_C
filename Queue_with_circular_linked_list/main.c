775c861aca1967e359a0cf0c34a039c7a0093b70
Ian <aa01912001@gmail.com>
2020-02-08 16:18:26 +0800

    add comments

--- Queue_with_circular_linked_list/main.c
+++ Queue_with_circular_linked_list/main.c
@@ -6,12 +6,12 @@ Object: To implement queue with circular linked list in C.
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node{
+typedef struct node{ // define node structure
     int data;
     struct node *link;
 }node;
 
-typedef struct queue{
+typedef struct queue{ // define queue structure
     node *rear;
 }queue;
 
@@ -20,19 +20,19 @@ int isEmpty(queue *);
 void enqueue(queue *, int);
 int dequeue(queue *);
 
-void init_queue(queue *q){
+void init_queue(queue *q){ //assign NULL to rear
     q->rear = NULL;
     return;
 }
 
-int isEmpty(queue *q){
+int isEmpty(queue *q){ // determine if queue is empty
     return (q->rear == NULL);
 }
 
-void enqueue(queue *q, int item){
+void enqueue(queue *q, int item){ // insert an item into queue
     node *tmp = (node *)malloc(sizeof(node));
     tmp->data = item;
-    if(q->rear == NULL){
+    if(q->rear == NULL){ // In case queue is currently empty.
         tmp->link = tmp;
     }else{
         tmp->link = q->rear->link;
@@ -43,7 +43,7 @@ void enqueue(queue *q, int item){
     return;
 }
 
-int dequeue(queue *q){
+int dequeue(queue *q){ // delete an item from queue
     if(isEmpty(q)){
         printf("Queue is empty!!\n");
         exit(1);
@@ -73,16 +73,16 @@ int main()
 
 
     int i;
-    for(i=1; i<=100; i++){
+    for(i=1; i<=100; i++){ // insert 100 items into queue
         enqueue(q, i);
     }
 
     printf("----------------------------\n");
 
-    while(!isEmpty(q)){
+    while(!isEmpty(q)){ // remove all items from queue
         dequeue(q);
     }
 
-    free(q);
+    free(q); // release memory of queue
     return 0;
 }
